38a09089634d81c8d75cba1a6e5c35414b87b730
FunctionJoo <joojy0914@gmail.com>
2022-04-21 17:11:59 +0900

    test

---
+++ src/views/GameList.vue
@@ -0,0 +1,46 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import { useStore } from '@/stores/store'
+
+let data = {
+  playerId : "a52d4f6e8e4303f9e59c46354d2b92ba",
+  nickname : "사이퍼즈개핵노잼",
+  grade : 74,
+  clanName : "ㅡ중독ㅡ",
+  ratingPoint : null,
+  maxRatingPoint : null,
+  tierName : null,
+  records : [ {
+    gameTypeId : "rating",
+    winCount : 0,
+    loseCount : 0,
+    stopCount : 0
+  }, {
+    gameTypeId : "normal",
+    winCount : 82,
+    loseCount : 111,
+    stopCount : 0
+  } ]
+};
+
+export default {
+  setup() {
+    const store = useStore();
+		console.log(store.apiKey);
+    fetch(`https://api.neople.co.kr/cy/players/${store.testId}?apikey=${store.apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  },
+}
+</script>
+
+<template>
+  <div class="game-list">
+		List
+	</div>
+</template>
+
+<style scoped>
+</style>
